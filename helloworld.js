723cf10ee4ed6b67764a5996160b2ab8af2eecee
masayuki <itasoba@gmail.com>
2013-09-09 17:09:33 +0900

    Google Maps API

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,22 @@
 var PORT = 1234;
 var IP = '127.0.0.1';
 var http = require('http');
-http.createServer(function(request, response) {
- response.writeHead(200, { 'Content-Type': 'text/plain' });
- response.end('Hello World!\n');
-}).listen(PORT, IP);
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+ // google maps.
+ if (req.url == '/map') {
+  fs.readFile('./html/map.html', function(err, data) {
+   res.writeHead(200, { 'Content-Type': 'text/html' });
+   res.end(data);
+  });
+ // hello world.
+ } else {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello World!\n');
+  res.end('Google Map API -> http://' + IP + ':' + PORT + '/map');
+ }
+}).listen(PORT, IP, function(){
+ console.log("Server is running at http://" + IP + ":" + PORT + "/");
+});
 
